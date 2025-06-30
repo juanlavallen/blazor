b63941a181ad306697b74ce97320052741b7469c
Juan Lavallen <juan.lavallen.dev@gmail.com>
2025-06-29 23:33:32 -0300

    feat: add TasklyItem model

---
+++ taskly/Taskly/Models/TasklyItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Taskly.Models;
+
+public class TasklyItem
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string Summary { get; set; } = default!;
+    public string Priority { get; set; } = default!;
+    public bool IsDone { get; set; }
+    public DateTime? DueDate { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
